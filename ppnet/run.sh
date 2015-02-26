8a5123142770c815cbb083a0e382d9beef5d124c
mlopezr <maski.net@gmail.com>
2015-02-26 01:37:44 +0100

    Correcting path of config.json in container

--- ppnet/run.sh
+++ ppnet/run.sh
@@ -6,7 +6,7 @@ if [ "$COUCHDB_URL" ]; then
 	echo "Configuring webapp to use CouchDB at IP $COUCHDB_URL"
 	ESCAPED_COUCHDB_URL=$(echo $COUCHDB_URL | sed -e 's/[\/&]/\\&/g')
 	ESCAPED_DEMO_COUCHDB_URL=$(echo $DEMO_COUCHDB_URL | sed -e 's/[\/&]/\\&/g')
-	sed -i "s/$ESCAPED_DEMO_COUCHDB_URL/$ESCAPED_COUCHDB_URL/" /usr/html/config.json
+	sed -i "s/$ESCAPED_DEMO_COUCHDB_URL/$ESCAPED_COUCHDB_URL/" /usr/ppnet-master/www/config.json
 else
 	echo "Using demo CouchDB server at $DEMO_COUCHDB_URL"
 fi
