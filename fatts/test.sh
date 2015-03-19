8143ec95c04eaa34369c82abc26cf1ce04faf531
Giulio Paci <giuliopaci@gmail.com>
2015-03-19 14:56:05 +0100

    Added smoke test.sh script for FA-TTS.

---
+++ fatts/test.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+if ! test -z "$1"
+then
+    HOST="$1"
+else
+    HOST="localhost"
+fi
+
+if ! test -z "$2"
+then
+    BASEPORT="$2"
+else
+    BASEPORT="59125"
+fi
+
+ENABLER_NAME="FA-TTS SE"
+
+echo "Entering $ENABLER_NAME smoke test sequence. $ENABLER_NAME validation procedure engaged. Target host: $HOST, port: $BASEPORT"
+
+
+REQUEST_URL=/info/version
+echo "Run smoke test for $REQUEST_URL"
+
+ITEM_RESULT=`curl -s -o /dev/null -w "%{http_code}" http://"$HOST:$BASEPORT$REQUEST_URL"`
+if [ "$ITEM_RESULT" -ne "200" ]; then
+        echo "Curl command for $REQUEST_URL failed. Validation procedure terminated."
+        echo "Debug information: HTTP code $ITEM_RESULT instead of expected 200 from $HOST:$BASEPORT"
+        exit 1;
+else
+        echo "Curl command for $REQUEST_URL OK."
+fi
+
+REQUEST_URL=/info/voices/all
+echo "Run smoke test for $REQUEST_URL"
+
+ITEM_RESULT=`curl -s -o /dev/null -w "%{http_code}" http://"$HOST:$BASEPORT$REQUEST_URL"`
+if [ "$ITEM_RESULT" -ne "200" ]; then
+        echo "Curl command for $REQUEST_URL failed. Validation procedure terminated."
+        echo "Debug information: HTTP code $ITEM_RESULT instead of expected 200 from $HOST:$BASEPORT"
+        exit 1;
+else
+        echo "Curl command for $REQUEST_URL OK."
+fi
+
+VOICE_ID=`curl -s http://"$HOST:$BASEPORT$REQUEST_URL" | sed -e 's/.*"id":"\([^"]*\)".*/\1/' | head -n 1`
+REQUEST_URL=/info/voice/"$VOICE_ID"/inputs/all
+echo "Run smoke test for $REQUEST_URL"
+
+ITEM_RESULT=`curl -s -o /dev/null -w "%{http_code}" http://"$HOST:$BASEPORT$REQUEST_URL"`
+if [ "$ITEM_RESULT" -ne "200" ]; then
+        echo "Curl command for $REQUEST_URL failed. Validation procedure terminated."
+        echo "Debug information: HTTP code $ITEM_RESULT instead of expected 200 from $HOST:$BASEPORT"
+        exit 1;
+else
+        echo "Curl command for $REQUEST_URL OK."
+fi
+
+echo "Smoke test completed. $ENABLER_NAME validation procedure succeeded. Over."
